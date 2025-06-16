208bc829fa6306f3a9d7e405e04d8a1140f9ccd1
hsy822 <hyunsy822@gmail.com>
2025-06-16 20:40:21 +0900

    update portal url

--- packages/sdk/dist/constants.d.ts
+++ packages/sdk/dist/constants.d.ts
@@ -1,3 +1,3 @@
-export declare const PROOF_PORTAL_URL = "https://zk-coinbase-attestor-proof-portal.vercel.app/";
+export declare const PROOF_PORTAL_URL = "https://proofportal.vercel.app/";
 export declare const CIRCUIT_URL = "https://raw.githubusercontent.com/hsy822/zk-coinbase-attestor/main/packages/circuit/target/zk_coinbase_attestor.json";
-export declare const ALLOWED_ORIGIN = "https://zk-coinbase-attestor-proof-portal.vercel.app/";
+export declare const ALLOWED_ORIGIN = "https://proofportal.vercel.app/";
--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,4 +1,4 @@
-export const PROOF_PORTAL_URL = "https://zk-coinbase-attestor-proof-portal.vercel.app/";
+export const PROOF_PORTAL_URL = "https://proofportal.vercel.app/";
 export const CIRCUIT_URL =
   "https://raw.githubusercontent.com/hsy822/zk-coinbase-attestor/main/packages/circuit/target/zk_coinbase_attestor.json";
-export const ALLOWED_ORIGIN = "https://zk-coinbase-attestor-proof-portal.vercel.app/";
+export const ALLOWED_ORIGIN = "https://proofportal.vercel.app/";
