261160ace74c29e66c54e55cc14743c97d92e940
hsy822 <hyunsy822@gmail.com>
2025-07-01 13:50:00 +0900

    update portal url

--- packages/sdk/dist/constants.js
+++ packages/sdk/dist/constants.js
@@ -1,6 +1,6 @@
-export const PROOF_PORTAL_URL = "https://silly-taiyaki-c12567.netlify.app/";
+export const PROOF_PORTAL_URL = "https://proofportal.netlify.app/";
 // export const PROOF_PORTAL_URL = "http://localhost:3000";
 export const CIRCUIT_URL = "https://raw.githubusercontent.com/hsy822/zk-coinbase-attestor/develop/packages/circuit/target/zk_coinbase_attestor.json";
-export const ALLOWED_ORIGIN = "https://silly-taiyaki-c12567.netlify.app/";
+export const ALLOWED_ORIGIN = "https://proofportal.netlify.app/";
 // export const ALLOWED_ORIGIN = "http://localhost:3000";
 export const COINBASE_CONTRACT = "0x357458739F90461b99789350868CD7CF330Dd7EE";
